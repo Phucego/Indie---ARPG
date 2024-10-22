9bbe5e9cf5c00f8b1defb1a237008e11f2905778
Phucego <phucdinhthanh2312@gmail.com>
2024-10-22 20:59:53 +0700

    22/10

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : Singleton<PlayerMovement>
 {
     //input fields
     private PlayerInputActions _playerInputActions;
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject singletonObj = new GameObject(typeof(T).Name);
+                    instance = singletonObj.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
