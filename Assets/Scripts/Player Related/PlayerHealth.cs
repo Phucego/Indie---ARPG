7bf8f908cf36fc20964f17b3f9aba284b50714cc
Phucego <phucdinhthanh2312@gmail.com>
2025-02-25 12:02:46 +0700

    25/2

--- Assets/Scripts/Player Related/PlayerHealth.cs
+++ Assets/Scripts/Player Related/PlayerHealth.cs
@@ -1,69 +1,45 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Random = System.Random;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
-
     [SerializeField] public float currentHealth;
     [SerializeField] public float maxHealth;
     [SerializeField] private float collisionDamageTaken;
     [SerializeField] private float lerpSpeed;
-    [SerializeField] private float baseHealthBarWidth; // Default width for a base max health
-    
-    /*
-    private float damageTimer = 0f;           // Track time since last damage
-    private float damageCooldown = 1f;        // Cooldown duration in seconds
-    */
-    
-    
-    public float healthBarWidth;
+    [SerializeField] private float baseHealthBarWidth;
 
-    [SerializeField] private float xPivot;
-    [SerializeField] private float yPivot;
-     
-    public Slider healthSlider;
-    public Slider easeHealthSlider;
+    public Slider healthBar;
+    public GameObject staminaBar;
+    public GameObject damageTextPrefab;
+    private Vector3 originalStaminaBarScale;
 
     private Animator animator;
     public string currentAnimation = "";
-
+    
     public PlayerHealth instance;
     PlayerMovement _playerMovement;
-    // Start is called before the first frame update
+    
     void Start()
     {
         currentHealth = maxHealth;
         instance = this;
-        healthSlider.value = maxHealth;
         
         animator = GetComponent<Animator>();
+        _playerMovement = GetComponent<PlayerMovement>();  
 
-        _playerMovement = GetComponent<PlayerMovement>();   
-        UpdateHealthBarSize();
+        originalStaminaBarScale = staminaBar.transform.localScale;
+
+        
+        UpdateHealthBar();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //TODO: Setting the max value of the slider equal to the max health
-        healthSlider.maxValue = maxHealth;
-        easeHealthSlider.maxValue = maxHealth;
-        
-        if (healthSlider.value != currentHealth)
-        {
-            healthSlider.value = currentHealth;
-        }
-
-        if (healthSlider.value != easeHealthSlider.value)
-        {
-            easeHealthSlider.value = Mathf.Lerp(easeHealthSlider.value, currentHealth, lerpSpeed);
-        }
-
         DestroyObject();
+        UpdateHealthBarSize();
     }
 
     #region Damage Taking and Death
@@ -78,13 +54,15 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         {
             currentHealth -= damage;
         }
+        
+        UpdateHealthBar();
+        StartCoroutine(ShowDamageNumber(damage));
     }
 
     public void DestroyObject()
     {
         if (currentHealth <= 0)
         {
-            //currentHealth = 0;
             Destroy(gameObject);
         }
     }
@@ -92,39 +70,46 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     private void OnCollisionEnter(Collision other)
     {
         if (!other.collider.name.Contains("Enemy_")) return;
-    
-        /*// Check if enough time has passed since last damage
-        if (Time.time >= damageTimer)
-        {
-            TakeDamage(collisionDamageTaken);
-            _playerMovement.ChangeAnimation("Player_GotHit");
-            damageTimer = Time.time + damageCooldown;    // Set next allowed damage time
-        }*/
     }
 
     #endregion
 
+    private void UpdateHealthBar()
+    {
+        if (healthBar != null)
+        {
+            healthBar.value = currentHealth / maxHealth;
+        }
+    }
 
     private void UpdateHealthBarSize()
     {
-        float healthBarWidth = baseHealthBarWidth * maxHealth; // Scale width based on maxHealth
-
-        RectTransform healthRectTransform = healthSlider.GetComponent<RectTransform>();
-        RectTransform easeHealthRectTransform = easeHealthSlider.GetComponent<RectTransform>();
-
-        // Set anchor points and pivot to (0, 0.5) to expand from the left
-        healthRectTransform.anchorMin = new Vector2(0, 0.5f);
-        healthRectTransform.anchorMax = new Vector2(0, 0.5f);
-        healthRectTransform.pivot = new Vector2(0, 0.5f);
-
-        easeHealthRectTransform.anchorMin = new Vector2(0, 0.5f);
-        easeHealthRectTransform.anchorMax = new Vector2(0, 0.5f);
-        easeHealthRectTransform.pivot = new Vector2(0, 0.5f);
-
-        // Update size to expand from the left side
-        healthRectTransform.sizeDelta = new Vector2(healthBarWidth, healthRectTransform.sizeDelta.y);
-        easeHealthRectTransform.sizeDelta = new Vector2(healthBarWidth, easeHealthRectTransform.sizeDelta.y);
-
+        if (healthBar != null)
+        {
+            RectTransform healthBarRect = healthBar.GetComponent<RectTransform>();
+            healthBarRect.sizeDelta = new Vector2(baseHealthBarWidth * (maxHealth * 0.626f), healthBarRect.sizeDelta.y);
+        }
     }
 
+    private IEnumerator ShowDamageNumber(float damage)
+    {
+        GameObject damageText = Instantiate(damageTextPrefab, transform.position, Quaternion.identity);
+        Text damageTextComponent = damageText.GetComponent<Text>();
+        damageTextComponent.text = damage.ToString();
+        Color textColor = damageTextComponent.color;
+
+        float duration = 1f;
+        float elapsedTime = 0f;
+        
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            textColor.a = Mathf.Lerp(1, 0, elapsedTime / duration);
+            damageTextComponent.color = textColor;
+            damageText.transform.position += Vector3.up * Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(damageText);
+    }
 }
--- Assets/Scripts/Player Related/StaminaManager.cs
+++ Assets/Scripts/Player Related/StaminaManager.cs
@@ -13,20 +13,19 @@ public class StaminaManager : MonoBehaviour
     
     
     public Slider staminaSlider;
-    public Slider easeHealthSlider;
-    
+   
     [SerializeField] private float lerpSpeed;
     void Start()
     {
         currentStamina = maxStamina;  // Initialize stamina to max
-        UpdateStaminaBarSize();
+        //UpdateStaminaBarSize();
     }
 
     void Update()
     {
         //TODO: Setting the max value of the slider equal to the max stamina
         staminaSlider.maxValue = maxStamina;
-        easeHealthSlider.maxValue = maxStamina;
+      
         if (Time.time >= lastStaminaUseTime + regenCooldown)
         {
             RegenerateStamina();
@@ -34,11 +33,6 @@ public class StaminaManager : MonoBehaviour
         if (staminaSlider.value != currentStamina)
         {
             staminaSlider.value = currentStamina;
-        }
-
-        if (staminaSlider.value != easeHealthSlider.value)
-        {
-            easeHealthSlider.value = Mathf.Lerp(easeHealthSlider.value, currentStamina, lerpSpeed);
         }
     }
 
@@ -58,26 +52,16 @@ public class StaminaManager : MonoBehaviour
     {
         currentStamina = Mathf.Min(currentStamina + staminaRegenRate * Time.deltaTime, maxStamina);
     }
-    private void UpdateStaminaBarSize()
+    /*private void UpdateStaminaBarSize()
     {
         float staminaBarWidth = baseStaminaBarWidth * maxStamina; // Scale width based on maxHealth
 
         RectTransform staminaRectTransform = staminaSlider.GetComponent<RectTransform>();
-        RectTransform easeHealthRectTransform = easeHealthSlider.GetComponent<RectTransform>();
-
         // Set anchor points and pivot to (0, 0.5) to expand from the left
         staminaRectTransform.anchorMin = new Vector2(0, 0.5f);
         staminaRectTransform.anchorMax = new Vector2(0, 0.5f);
         staminaRectTransform.pivot = new Vector2(0, 0.5f);
 
-        easeHealthRectTransform.anchorMin = new Vector2(0, 0.5f);
-        easeHealthRectTransform.anchorMax = new Vector2(0, 0.5f);
-        easeHealthRectTransform.pivot = new Vector2(0, 0.5f);
-
-        // Update size to expand from the left side
-        staminaRectTransform.sizeDelta = new Vector2(staminaBarWidth, staminaRectTransform.sizeDelta.y);
-        easeHealthRectTransform.sizeDelta = new Vector2(staminaBarWidth, easeHealthRectTransform.sizeDelta.y);
-
-    }
+    }*/
     
 }
